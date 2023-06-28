fce92693a38494162aa40688952eb87f5057170d
Mario Jose Ramos Mejia <marioramosmejia2243@gmail.com>
2023-06-28 02:24:40 -0600

    Pipes de Date, Idioma, tambien uppercase, lowercase y el capitalizador Implementando cambio de idioma por defecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
+import  localEsNI  from "@angular/common/locales/es-NI";
+import  localFrCA  from "@angular/common/locales/fr-CA";
+
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localEsNI);
+registerLocaleData(localFrCA);
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide : LOCALE_ID,useValue: "es-NI"
+    } 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -9,4 +9,6 @@ export class BasicsPageComponent {
   public nameLower : string ='mario';
   public nameUpper : string = 'RAMOS';
   public fullName : string = 'MarIO RamoS'; 
+
+  public dateNow : Date = new Date();
 }
